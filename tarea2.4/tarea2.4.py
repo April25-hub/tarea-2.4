a58388d84b13a7f0d8f2d3bb483ee016f9a9a0b9
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-09-24 20:26:35 -0600

    Add files via upload

---
+++ tarea2.4/tarea2.4.py
@@ -0,0 +1,22 @@
+#esta linea indicara mi nombre
+
+#Arzaba Diaz April
+
+#esta linea indica el numero de programa que are
+
+#Programa (tarea 2.4)
+
+num1 = float(input("ingresa el primer numero:"))
+
+num2 = float(input("ingresa el segundo numero:"))
+
+num3 = float(input("ingresa el tercee numero:"))
+
+
+#esta linea calcula el promedio de los tres numeros
+
+promedio = (num1 + num2 + num3)/ 3
+
+#esta linea me muestra el promedio de los tres numeros
+
+print("el promedio de los tres numeros es:",promedio)
